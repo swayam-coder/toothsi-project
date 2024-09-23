12638185613fcd9d64e1ad5e6b6c02b49a7113d2
swayam-coder <swayam14feb@gmail.com>
2024-09-23 19:33:44 +0530

    change

--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -3,9 +3,9 @@ import CartTable from '../components/CartTable';
 import CartBox from '../components/CartBox';
 
 const Checkout: React.FC = () => {
-  return <Row gutter={16}>
-    <Col span={18}><CartTable /></Col>
-    <Col span={6}><CartBox /></Col>
+  return <Row gutter={[16, 16]}>
+    <Col xs={24} sm={24} md={18} span={18}><CartTable /></Col>
+    <Col xs={24} sm={24} md={6} span={6}><CartBox /></Col>
   </Row>;
 };
 
